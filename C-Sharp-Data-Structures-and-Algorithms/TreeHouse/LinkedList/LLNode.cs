74f0adedca9d7165910826641057572408b19aba
Richard <richardwjmorrow@gmail.com>
2020-01-09 12:51:12 -0800

    part of some exercises

---
+++ C-Sharp-Data-Structures-and-Algorithms/TreeHouse/LinkedList/LLNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LLNode
+    {
+        int data;
+        LLNode next;
+
+        public LLNode(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
---
+++ C-Sharp-Data-Structures-and-Algorithms/TreeHouse/LinkedList/LinkedList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        LLNode head;
+        public LinkedList(int data)
+        {
+            head = new LLNode(data);
+        }
+
+
+    }
+
+}
